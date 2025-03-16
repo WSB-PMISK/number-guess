ebf47c590cd7fc3db16b82cc66c0a6f562ea9b4c
czak <czakcbprogramming@gmail.com>
2025-03-17 00:09:25 +0100

    add UserHandler

--- PMISK.Tests/UserHandleTest.cs
+++ PMISK.Tests/UserHandleTest.cs
@@ -1,11 +1,30 @@
+using System.Runtime.CompilerServices;
+using Moq;
+
 namespace PMISK.Tests;
 
 public class UserHandleTest
 {
-
+    
     [Fact]
-    public void UserHandleConstructorTest()
-    {
-        Assert.Throws<NotImplementedException>(() => new UserHandle());
+    public void AskTest() {
+        var input = "  input  ";
+        var expectedResult = "input";
+
+        var mockUserHandle = new Mock<IHandler>();
+        mockUserHandle.Setup(line => line.ReadLine()).Returns(input);
+
+        var userHandler = new UserHandler(mockUserHandle.Object);
+        
+        Assert.Equal(userHandler.Ask("q"), expectedResult);
+        mockUserHandle.Verify(mock => mock.ReadLine(), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(new string[] {"test", "choice"}, "test\nchoice")]
+    public void GetChoiceListTest(string[] choices, string expectedResult) {
+        var handler = new Handler();
+        var userHandler = new UserHandler(handler);
+        Assert.Equal(userHandler.GetChoiceList(choices), expectedResult);
     }
 }
--- PMISK/UserHandle.cs
+++ PMISK/UserHandle.cs
@@ -1,9 +1,30 @@
 namespace PMISK;
 
-public class UserHandle
-{
-    public UserHandle()
-    {
-        throw new NotImplementedException("Class not implemented yet");
+public interface IHandler {
+    public string ReadLine();
+}
+
+public class Handler : IHandler {
+    public string ReadLine() {
+        var readLine = Console.ReadLine()!.Trim();
+        return readLine;
+    }
+}
+
+public class UserHandler(IHandler handler) {
+    public string Ask(string question) {
+        Console.Write($"[ PYTANIE ] {question}: ");
+        var answer = handler.ReadLine().Trim();
+
+        if (string.IsNullOrEmpty(answer)) {
+            Console.WriteLine("Prosze podac odpowiedz");
+            answer = Ask(question);
+        }
+
+        return answer;
+    }
+
+    public string GetChoiceList(string[] choices) {
+        return string.Join("\n", choices);
     }
 }
