41f942e05c3900254824f4e324dbe92aca883181
czakk <czakcbprogramming@gmail.com>
2025-03-18 21:19:45 +0100

    Add UserHandler (#3)

--- PMISK.Tests/UserHandleTest.cs
+++ PMISK.Tests/UserHandleTest.cs
@@ -1,11 +1,31 @@
+using System.Runtime.CompilerServices;
+using Moq;
+
 namespace PMISK.Tests;
 
 public class UserHandleTest
 {
-
+    
     [Fact]
-    public void UserHandleConstructorTest()
-    {
-        Assert.Throws<NotImplementedException>(() => new UserHandle());
+    public void AskTest() {
+        var input = "  input  ";
+        var expectedResult = "input";
+
+        var mockUserHandle = new Mock<IHandler>();
+        mockUserHandle.Setup(line => line.ReadLine()).Returns(input);
+
+        var userHandler = new UserHandler(mockUserHandle.Object);
+        
+        Assert.Equal(userHandler.Ask("q"), expectedResult);
+        mockUserHandle.Verify(mock => mock.ReadLine(), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(new string[] {"test", "choice"}, "  1. test\n  2. choice")]
+    [InlineData(new string[] {"more", "test", "choices"}, "  1. more\n  2. test\n  3. choices")]
+    public void GetChoiceListTest(string[] choices, string expectedResult) {
+        var handler = new Handler();
+        var userHandler = new UserHandler(handler);
+        Assert.Equal(expectedResult, userHandler.GetChoiceList(choices));
     }
 }
--- PMISK/UserHandle.cs
+++ PMISK/UserHandle.cs
@@ -1,9 +1,36 @@
 namespace PMISK;
 
-public class UserHandle
-{
-    public UserHandle()
-    {
-        throw new NotImplementedException("Class not implemented yet");
+public interface IHandler {
+    public string ReadLine();
+}
+
+public class Handler : IHandler {
+    public string ReadLine() {
+        var readLine = Console.ReadLine()!.Trim();
+        return readLine;
+    }
+}
+
+public class UserHandler(IHandler handler) {
+    public string Ask(string question) {
+        Console.Write($"[ PYTANIE ] {question}: ");
+        var answer = handler.ReadLine().Trim();
+
+        if (string.IsNullOrEmpty(answer)) {
+            Console.WriteLine("Prosze podac odpowiedz");
+            answer = Ask(question);
+        }
+
+        return answer;
+    }
+    
+    public string GetChoiceList(string[] choices) {
+        var list = "";
+
+        for (var i = 1;i<=choices.Length;i++) {
+            list += $"  {i}. {choices[i-1]}\n";
+        }
+
+        return list.Substring(0, list.Length - 1);
     }
 }
